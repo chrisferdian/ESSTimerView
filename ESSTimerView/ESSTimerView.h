d8af83f4a420c424350702631113f5dc53384554
Chris Ferdian <chris.ferdian@gmail.com>
2019-03-12 20:38:37 +0700

    Initial Commit

---
+++ ESSTimerView/ESSTimerView.h
@@ -0,0 +1,19 @@
+//
+//  ESSTimerView.h
+//  ESSTimerView
+//
+//  Created by Chris Ferdian on 12/03/19.
+//  Copyright © 2019 Chris Ferdian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESSTimerView.
+FOUNDATION_EXPORT double ESSTimerViewVersionNumber;
+
+//! Project version string for ESSTimerView.
+FOUNDATION_EXPORT const unsigned char ESSTimerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESSTimerView/PublicHeader.h>
+
+
